519788359fa4c0f8165e8b63990a9f9ca3c2811e
Qingge <qingge_kang@limei-trading.com>
2017-03-30 18:31:10 +0800

    templates and login code

---
+++ conn/conn.php
@@ -0,0 +1,6 @@
+<?php
+$conn = @mysql_connect("localhost","root","root");
+mysql_select_db("db_forum",$conn);
+mysql_query("set names utf-8");
+
+
---
+++ login.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>注册</title>
+    <style type="text/css">
+        <!--
+        body,td,th {
+            font-size: 12px;
+        }
+        .STYLE1 {color: #FF0000}
+        -->
+    </style>
+</head>
+<body>
+<form name="form1" action="" method="post" >
+    <table>
+        <tr>
+            <td>用户名:</td>
+            <td><input name="username" type="text" id="username"/></td>
+        </tr>
+        <tr>
+            <td>密码：</td>
+            <td><input type="password" name="password" id="password"/></td>
+        </tr>
+        <tr>
+            <td>确认密码：</td>
+            <td><input type="password" name="password2" id="password2"/></td>
+        </tr>
+        <tr>
+            <td>性别:</td>
+            <td>
+                <input type="radio" name="sex" id="sex" checked/>男
+                <input type="radio" name="sex" id="sex"/>女
+            </td>
+        </tr>
+        <tr>
+            <td>联系电话:</td>
+            <td><input name="tel" type="text" id="tel"/></td>
+        </tr>
+        <tr>
+            <td>QQ:</td>
+            <td><input name="qq" type="text" id="qq"/></td>
+        </tr>
+        <tr></tr>
+        <tr>
+            <td>Email:</td>
+            <td><input name="email" type="text" id="email"/></td>
+        </tr>
+        <tr>
+            <td>个人主页:</td>
+            <td><input name="indexs" type="text" id="indexs"/></td>
+        </tr>
+        <tr>
+            <td>地址:</td>
+            <td><input name="address" type="text" id="address"/></td>
+        </tr>
+        <tr>
+            <td>
+                <input type="submit" name="Submit" value="确认提交"/>
+                <input type="reset" name="Reset" value="刷新重置"/>
+            </td>
+        </tr>
+    </table>
+</form>
+
+
+
+
+
+
+
+</body>
+
+
+
+
+
