a79246a0c37f93a03adb7248edf238816ef2bed3
qingge <qgkang@163.com>
2017-03-30 23:42:54 +0800

    select avantar code

--- login.php
+++ login.php
@@ -13,6 +13,76 @@
     </style>
 </head>
 <body>
+<script language="javascript">
+    function checkit(){
+        if(form1.username.value == ""){
+            alert("请输入用户名!");
+            form1.username.select();
+            return(false);
+        }
+        if(form1.password.value==""){
+            alert("请输入用户密码!");
+            form1.password.select();
+            return(false);
+        }
+        if(form1.password.value!=form1.password2.value){
+            alert("您输入的密码和验证密码不符！");
+            form1.password2.select();
+            return(false);
+        }
+
+        if(form1.tel.value==""){
+            alert("请输入电话号码!");
+            form1.tel.select();
+            return(false);
+        }
+        if(!checkphone(form1.tel.value)){
+            alert("电话号码格式不正确!");
+            form1.tel.select();
+            return(false);
+        }
+
+        if(form1.email.value==""){
+            alert("请输入邮箱地址!");
+            form1.email.select();
+            return(false);
+        }
+        if(!checkemail(form1.email.value)){
+            alert("邮箱地址格式不正确!");
+            form1.email.select();
+            return(false);
+        }
+        return(true);
+    }
+
+    function checkemail(email){
+        var strs=email;
+        var Expression=/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+        var objExp=new RegExp(Expression);
+        if(objExp.test(strs)==true){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    function checkphone(tel){
+        var str=tel;
+        var Expression=/^(\d{3}-)(\d{8})$|^(\d{4}-)(\d{7})$|^(\d{4}-)(\d{8})$|^(\d{11})$/;
+        var objExp=new RegExp(Expression);
+        if(objExp.test(str)==true){
+            return true;
+        }else{
+            return false;
+        }
+    }
+<?php
+include("conn/conn.php");
+include("index_01.php");
+include("index_02.php");
+?>
+
+</script>
 <form name="form1" action="" method="post" >
     <table>
         <tr>
@@ -42,7 +112,30 @@
             <td>QQ:</td>
             <td><input name="qq" type="text" id="qq"/></td>
         </tr>
-        <tr></tr>
+        <script language="javascript">//通过下拉列表选择头像时应用该函数
+            function showlogo(){
+                document.images.img.src="images/tx/"+
+                    document.form1.tx.options[document.form1.tx.selectedIndex].value;
+            }
+        </script>
+        <tr>
+            <td>选择头像：</td>
+            <td>
+                <p><img src="images/tx/1.gif" id="img" name="img" width="60" height="60" /></p>
+                <select size"1" id="tx" name="tx" onChange="showlogo()">
+                <option value="1.gif">头像1</option>
+                <option value="2.gif">头像2</option>
+                <option value="3.gif">头像3</option>
+                <option value="4.gif">头像4</option>
+                <option value="5.gif">头像5</option>
+                <option value="6.gif">头像6</option>
+                <option value="7.gif">头像7</option>
+                <option value="8.gif">头像8</option>
+                <option value="9.gif">头像9</option>
+                <option value="10.gif">头像10</option>
+                </select>
+            </td>
+        </tr>
         <tr>
             <td>Email:</td>
             <td><input name="email" type="text" id="email"/></td>
